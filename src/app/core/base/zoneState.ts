fdc17c97646273657fdbc8ce3a3123e7d312a53f
luozesheng <zesheng_luo@foxitsoftware.cn>
2020-11-24 09:35:22 +0800

    '添加zone.js的代码测试案例'

---
+++ src/app/core/base/zoneState.ts
@@ -0,0 +1,58 @@
+import 'zone.js';
+export default class ZoneTest{
+	public name:string = "";
+	baseTest(){
+		let logZone = Zone.current.fork({
+			name: "logZone",
+			onInvoke: (
+				parentZoneDelegate, 
+				currentZone, 
+				targetZone, 
+				delegate, 
+				applyThis, 
+				applyArgs, 
+				source) => {
+				console.log(targetZone.name, "start");
+				parentZoneDelegate.invoke(targetZone, delegate, applyThis, applyArgs, source);
+				console.log(targetZone.name, 'end');
+			}
+		});
+		logZone.run( () => {
+			console.log(Zone.current.name, "queque promise");
+			Promise.resolve("ok").then(v => {
+				console.log(Zone.current.name, 'Promise', v)
+			});
+		});	
+	}
+	// 异步调度任务的处理
+	testScheduleLog(){
+		console.log("输出");
+		const executeTimeZoneSpec = {
+		  name: 'executeTimeZone',
+		  onScheduleTask: function (parentZoneDelegate, currentZone, targetZone, task) {
+		    console.time('scheduleTask')
+		    return parentZoneDelegate.scheduleTask(targetZone, task);
+		  },
+		  onInvokeTask: function (parentzone, currentZone, targetZone, task, applyThis, applyArgs) {
+		    console.time('callback')
+		    parentzone.invokeTask(targetZone, task, applyThis, applyArgs);
+		    console.timeEnd('callback')
+		    console.timeEnd('scheduleTask')
+		  }
+		}
+
+		Zone.current.fork(executeTimeZoneSpec).run(() => {
+		  setTimeout(function () {
+		    console.log('start callback...')
+		    for (let i = 0; i < 10; i++) {
+		      console.log(i)
+		    }
+		  }, 1000);
+		});
+
+	}
+	// 执行类函数入口
+	main(){
+		this.testScheduleLog()
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 // import HightlightText from './highlightText/index';
 import Events from './Events/index';
+import ZoneTest from './app/core/base/zoneState';
 const msg = '123456789';
 
+let zoneTest:ZoneTest = new ZoneTest();
+
 window.onload = () => {
-    let events = new Events();
-    events.on( "maria", (goodName, goodPrice) => {
-        console.log("这件商品的名称: " + goodName);
-        console.log("这件商品的价格: " + goodPrice);
-    });
-    events.emitter("maria", "玛丽亚", 156);
+    // let events = new Events();
+    // events.on( "maria", (goodName, goodPrice) => {
+    //     console.log("这件商品的名称: " + goodName);
+    //     console.log("这件商品的价格: " + goodPrice);
+    // });
+    // events.emitter("maria", "玛丽亚", 156);
     // let t = new HightlightText(document.getElementById('app'));
     // t.createText(msg);
     // document.getElementById('app').onmouseleave=()=>{
@@ -16,7 +19,8 @@ window.onload = () => {
     //         t.paint(0);
     //     }catch(e){
     //         throw new Error('未选中对应的高亮文本！')
-    //     }
-        
+    //     }        
     // }
+    // Zone单元测试
+    zoneTest.main();
 }
